f4722a9038cb29bfe3773b00892ddbf363726edc
Alan Alvarez <alanalvarez98.aa@gmail.com>
2020-05-22 20:26:19 -0500

    Instalación y configuración de path, cors, morgan, nodemon para el servidor

--- server/app.js
+++ server/app.js
@@ -1,11 +1,30 @@
 
-const express = require('express');
+import express from 'express';
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Computo en la nube');
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+// app.get('/', (req, res) => {
+//     res.send('Computo en la nube');
+// });
+
+import history from 'connect-history-api-fallback';
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Asignar puerto
+app.set('port', process.env.PORT || 3000);
+
+
+app.listen(app.get('port'), () => {
+    console.log('Server on ', app.get('port'));
 });
 
-app.listen(3000, () => {
-    console.log('Server on 3000')
-})
---
+++ server/readme.md
@@ -0,0 +1,23 @@
+## Run Server
+
+Dentro de la carpeta del repositorio.
+
+1.- Agregar cambios recientes
+
+```bash
+git pull
+```
+
+Ingresar a la carpeta del BackEnd Servidor
+
+2.- Instalar dependencias 
+
+```bash
+npm install
+```
+
+3.- Correr Servidor
+
+```bash
+npm run dev
+```
