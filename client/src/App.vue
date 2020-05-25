436fc3460d2b55f2f55555de1988f9b254fcf5b9
Oscar Montes <oscmcojc1@live.com.mx>
2020-05-25 16:12:48 -0500

    Persistencia de la sesión del usuario

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,12 +9,19 @@
 </template>
 
 <script>
+  import {mapActions} from 'vuex';
   import Navbar from "./components/Navbar"
   import SnackBar from "./components/SnackBar";
   export default {
     components: {
       Navbar,
       SnackBar
+    },
+    methods: {
+      ...mapActions(['tryAutoLogin'])
+    },
+    created() {
+      this.tryAutoLogin();
     }
   }
 </script>
