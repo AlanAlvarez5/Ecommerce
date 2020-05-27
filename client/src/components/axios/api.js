09666743d35b8fc1486fb281ab7f3898c7797f57
Oscar Montes <oscmcojc1@live.com.mx>
2020-05-27 18:01:24 -0500

    Hotfix production vars

--- client/src/components/axios/api.js
+++ client/src/components/axios/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const API = axios.create({
-  baseURL: process.env.PRODUCTION ? 'http://3.15.2.165/api' : 'http://localhost:3000/api',
+  baseURL: process.env.NODE_ENV === 'production' ? 'http://3.15.2.165/api' : 'http://localhost:3000/api',
   headers: {
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaGVjayI6dHJ1ZSwiYWRtaW4iOnRydWUsImlkIjoxLCJpYXQiOjE1OTA0NTAxODV9.K8TyBNi09PYwGn0Km9K558fRjG7Ywn_fnDVRIoJ29SA',
   }
