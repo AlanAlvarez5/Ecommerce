566eee8c57158a55db31447fa4199f9205b2c988
Christian <cristian117acosta@hotmail.com>
2020-05-28 13:34:03 -0500

    Modelos de lectura de compras

--- server/routes/compra.js
+++ server/routes/compra.js
@@ -2,23 +2,23 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 const router = express.Router();
 
-router.use((req, res, next) => {
-    if (req.headers.authorization) {
-        const token = req.headers.authorization.split(' ')[1];
-        jwt.verify(token, req.app.get('llave'), (err, decoded) => {
-            if (err) {
-                return res.json({ mensaje: 'Token invalido' });
-            } else {
-                req.decoded = decoded;
-                next();
-            }
-        });
-    } else {
-        res.send({
-            mensaje: 'Token no enviado'
-        });
-    }
-});
+// router.use((req, res, next) => {
+//     if (req.headers.authorization) {
+//         const token = req.headers.authorization.split(' ')[1];
+//         jwt.verify(token, req.app.get('llave'), (err, decoded) => {
+//             if (err) {
+//                 return res.json({ mensaje: 'Token invalido' });
+//             } else {
+//                 req.decoded = decoded;
+//                 next();
+//             }
+//         });
+//     } else {
+//         res.send({
+//             mensaje: 'Token no enviado'
+//         });
+//     }
+// });
 
 const db = require('../db');
 
@@ -34,6 +34,35 @@ router.get('/add', async(req, res) => {
         res.json({
             mensaje: 'COMPRA_ADDED'
         });
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'Query Error',
+            error
+        });
+    }
+});
+
+router.get('/select', async(req, res) => {
+    try {
+        // let usuario_id = req.recoded.id;
+        let usuario_id = 1;
+
+        let compras = await db.query(`SELECT * from compra where usuario_id = ${usuario_id}`);
+        res.json(compras);
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'Query Error',
+            error
+        });
+    }
+});
+
+router.get('/select/:id', async(req, res) => {
+    try {
+        let id = req.params.id;
+        // let id = 6;
+        let record = await db.query(`SELECT * from compra where id = ${id}`);
+        res.json(record);
     } catch (error) {
         return res.status(400).json({
             mensaje: 'Query Error',
