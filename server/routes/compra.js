a845808207f66fe44ad94832f8bf0a2829dc32db
Christian <cristian117acosta@hotmail.com>
2020-05-28 10:42:07 -0500

    Modelo add para compra

--- server/routes/compra.js
+++ server/routes/compra.js
@@ -2,18 +2,18 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 const router = express.Router();
 
-router.use((req,res, next) => {
-    if(req.headers.authorization) {
+router.use((req, res, next) => {
+    if (req.headers.authorization) {
         const token = req.headers.authorization.split(' ')[1];
         jwt.verify(token, req.app.get('llave'), (err, decoded) => {
             if (err) {
-                return res.json({mensaje: 'Token invalido'});
+                return res.json({ mensaje: 'Token invalido' });
             } else {
                 req.decoded = decoded;
                 next();
             }
         });
-    } else{
+    } else {
         res.send({
             mensaje: 'Token no enviado'
         });
@@ -22,6 +22,25 @@ router.use((req,res, next) => {
 
 const db = require('../db');
 
+router.get('/add', async(req, res) => {
+    try {
+        usuario_id = req.decoded.id
+            // let usuario_id = 3
+        let total = 0.0
+        let estado = 'Proceso'
+
+        let record = await db.query(`INSERT into compra (usuario_id, total, estado) values (${usuario_id}, ${total}, '${estado}')`)
+
+        res.json({
+            mensaje: 'COMPRA_ADDED'
+        });
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'Query Error',
+            error
+        });
+    }
+});
 
 
 
