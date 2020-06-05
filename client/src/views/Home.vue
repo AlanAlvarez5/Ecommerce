f1408576c66a3276882d98db92de2078ef698a0f
Oscar Montes <oscmcojc1@live.com.mx>
2020-06-05 18:09:35 -0500

    Hotfix

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -46,8 +46,10 @@
     async created() {
       const storage = JSON.parse(localStorage.getItem('cart'));
       let quantity = 0;
-      storage.forEach(el => quantity += el.quantity);
-      this.$store.state.UI.cartBadgeValue = quantity;
+      if(storage) {
+        storage.forEach(el => quantity += el.quantity);
+        this.$store.state.UI.cartBadgeValue = quantity;
+      }
       this.loadProducts();
     }
   }
