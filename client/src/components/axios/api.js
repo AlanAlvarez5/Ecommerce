1384faf1f8bbf63ea6dd1b97a979778b04b90d19
Oscar Montes <oscmcojc1@live.com.mx>
2020-06-05 21:15:01 -0500

    Nueva ip publica

--- client/src/components/axios/api.js
+++ client/src/components/axios/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 export const API = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'http://3.15.2.165/api' : 'http://localhost:3000/api',
+  baseURL: process.env.NODE_ENV === 'production' ? 'http://52.14.69.133/api' : 'http://localhost:3000/api',
 });
